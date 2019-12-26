118c9743a7522bdc1333b0163e5de30d608bf875
Niraj Kumar <Niraj.Kumar@rsystems.com>
2019-12-26 15:41:24 +0530

    Updated for addition of fetchsystem configuration on that machine console executed

--- GeoFancing/Hosts/ConsoleHost/Program.cs
+++ GeoFancing/Hosts/ConsoleHost/Program.cs
@@ -17,6 +17,8 @@ namespace ConsoleApp2
         {
             try
             {
+                FetchSystemConfiguration();
+
                 Dictionary<string, string> parameters = new Dictionary<string, string>();
                 parameters["RedisCacheConfig"] = ConfigurationManager.AppSettings["RedisCacheConfig"];
                 parameters["CosmoDatabaseName"] = ConfigurationManager.AppSettings["CosmoDatabaseName"];
@@ -71,6 +73,27 @@ namespace ConsoleApp2
                 }
             }
 
+        }
+
+        static void FetchSystemConfiguration()
+        {
+            
+            int lPathindex = System.Reflection.Assembly.GetExecutingAssembly().Location.LastIndexOf("\\");
+
+            string batchfilepath = Path.Combine(System.Reflection.Assembly.GetExecutingAssembly().Location.Substring(0, lPathindex), "Scripts\\Executepowershell.bat");
+            string ss = "powershell.exe " + Path.Combine(System.Reflection.Assembly.GetExecutingAssembly().Location.Substring(0, lPathindex), "Scripts\\PerformTest.ps1") + " " +
+                Path.Combine(System.Reflection.Assembly.GetExecutingAssembly().Location.Substring(0, lPathindex), "Scripts\\Systemconfig.txt");
+
+            StreamWriter streamWriter = new StreamWriter(batchfilepath);
+            streamWriter.Write(ss);
+            streamWriter.Close();
+            System.Diagnostics.Process.Start(batchfilepath).WaitForExit();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("********************************************************************");
+            Console.WriteLine("Performance Test executed on below system configurations: ");
+            Console.WriteLine(File.ReadAllText(Path.Combine(System.Reflection.Assembly.GetExecutingAssembly().Location.Substring(0, lPathindex), "Scripts\\Systemconfig.txt")).ToString()); ;
+            Console.WriteLine("********************************************************************");
+            Console.ResetColor();
         }
     }
 }
